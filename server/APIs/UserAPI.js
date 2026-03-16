cde4528f49be15b00371bfc5330a7ca88ea79e92
Uppala Anand <uppalaanand410@gmail.com>
2026-03-16 21:55:24 +0530

    change user password

--- server/APIs/UserAPI.js
+++ server/APIs/UserAPI.js
@@ -1,77 +1,165 @@
-import express from "express"
+import express from "express";
 import { UserModel } from "../Models/UserModel.js";
-import {hash, compare} from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import { hash, compare } from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 export const userRouter = express.Router();
 
-//Register the user
-userRouter.post("/register",async(req,res)=>{
-    let userObj = req.body;
+// Register the user
+userRouter.post("/register", async (req, res) => {
+  let userObj = req.body;
 
-    // document
-    let userDoc = new UserModel(userObj);
-    await userDoc.validate();
+  let userDoc = new UserModel(userObj);
 
-    // hash the password
-    userDoc.password = await hash(userDoc.password, 12);
-    //save
-    const created = await userDoc.save();
-    //convert document to object to remove password feild
-    const newUserObj = created.toObject();
-    //remove password
-    delete newUserObj.password;
-    //return respnse
-    res.status(201).json({message:"User created",payload:newUserObj});
+  await userDoc.validate();
+
+  userDoc.password = await hash(userDoc.password, 12);
+
+  const created = await userDoc.save();
+
+  const newUserObj = created.toObject();
+
+  delete newUserObj.password;
+
+  res.status(201).json({
+    message: "User created",
+    payload: newUserObj,
+  });
 });
 
-//login the user
+// Login the user
 userRouter.post("/login", async (req, res) => {
-    //get the user object from body
-    const newUserObj= req.body;
-    //check whether user exists
-    const user = await UserModel.findOne({email: newUserObj.email});
-    //if user not exists, ask user to register
-    if(!user) {
-        const err = new Error("Invalid email");
-        err.status = 401;
-        return;
-    }
-    //compare passwords
-    const isMatch = await compare(newUserObj.password, user.password);
-    // if password not matched, ask them them to enter correct message
-    if(!isMatch) {
-        const err = new Error("Invalid password");
-        err.status = 401;
-        return;
-    }
-    //generate token
-    const token = jwt.sign({userId:user._id, email: user.email}, process.env.JWT_SECRET, {expiresIn: '1h'});
+  const newUserObj = req.body;
 
-    //save the token in httpOnly
-    res.cookie("token", token, {
-        httpOnly : true,
-        sameSite : 'lax',
-        secure : false
+  const user = await UserModel.findOne({
+    email: newUserObj.email,
+  });
+
+  if (!user) {
+    return res.status(401).json({
+      message: "Invalid email",
     });
-    const userObj = user.toObject();
-    delete userObj.password;
-    //send res
-    res.status(200).json({message : "Login Success", payload : userObj});
-});
+  }
 
-// Search for a user by username
-userRouter.get("/search/:username", verifyToken, async (req, res) => {
-    try {
-        // Find the user by username, but exclude their password from the result
-        const user = await UserModel.findOne({ username: req.params.username }).select("-password");
+  const isMatch = await compare(
+    newUserObj.password,
+    user.password
+  );
 
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
+  if (!isMatch) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
 
-        res.status(200).json({ message: "User found", payload: user });
-    } catch (err) {
-        res.status(500).json({ error: "Failed to search user" });
+  const token = jwt.sign(
+    {
+      userId: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
     }
+  );
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: false,
+  });
+
+  const userObj = user.toObject();
+
+  delete userObj.password;
+
+  res.status(200).json({
+    message: "Login Success",
+    payload: userObj,
+  });
 });
+
+// Search user by username
+userRouter.get(
+  "/search/:username",
+  verifyToken,
+  async (req, res) => {
+    try {
+      const user = await UserModel.findOne({
+        username: req.params.username,
+      }).select("-password");
+
+      if (!user) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      }
+
+      res.status(200).json({
+        message: "User found",
+        payload: user,
+      });
+    } catch (err) {
+      res.status(500).json({
+        error: "Failed to search user",
+      });
+    }
+  }
+);
+
+// Change password
+userRouter.patch(
+  "/change-password",
+  verifyToken,
+  async (req, res) => {
+    const {
+      email,
+      currentPassword,
+      newPassword,
+    } = req.body;
+
+    let user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "User not found",
+      });
+    }
+
+    const isMatch = await compare(
+      currentPassword,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid password",
+      });
+    }
+
+    let createdNewPassword = await hash(
+      newPassword,
+      10
+    );
+
+    let updated = await UserModel.findOneAndUpdate(
+      { email },
+      {
+        $set: {
+          password: createdNewPassword,
+        },
+      },
+      { new: true }
+    );
+
+    const newUserObj = updated.toObject();
+
+    delete newUserObj.password;
+
+    res.status(200).json({
+      message: "Password Updated Successfully",
+      payload: newUserObj,
+    });
+  }
+);
