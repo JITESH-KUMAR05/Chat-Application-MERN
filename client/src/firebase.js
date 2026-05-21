d32baece0003ee9cd76075a7373d9bfaa596f489
Shivamani Burgu <shiva.burgu@gmail.com>
2026-05-22 01:13:06 +0530

    Complete Application Deployment

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPoRtmit5pUYdnAj9Z7UWVOyS11KSXHRc",
-  authDomain: "chat-application-615b6.firebaseapp.com",
-  projectId: "chat-application-615b6",
-  storageBucket: "chat-application-615b6.firebasestorage.app",
-  messagingSenderId: "536016826869",
-  appId: "1:536016826869:web:bf625876e9ffc48245f0bc",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-
-export const provider = new GoogleAuthProvider();
